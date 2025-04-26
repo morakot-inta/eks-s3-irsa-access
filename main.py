570a49287d634bf0c0084ebe7f43e0ec41a9e5b9
morakot <morakot.inta@gmail.com>
2025-04-26 17:02:20 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def main():
+    try:
+        s3 = boto3.client(
+            "s3", 
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            region_name=os.getenv("AWS_REGION")
+            )
+        bucket_name = os.getenv("AWS_BUCKET_NAME")
+        response = s3.list_objects_v2(Bucket=bucket_name)
+        print("Files in bucket:")
+        for obj in response.get('Contents', []):
+            print(obj['Key'])
+    except Exception as e:
+        print(f"Error initializing S3 client: {e}")
+        return
+
+
+if __name__ == "__main__":
+    main()
